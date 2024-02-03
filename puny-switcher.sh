ddc2eb1a104fa2545a264aaf113b51d6d66c1aa0
Kelly Roadkill <4406611+roadkell@users.noreply.github.com>
2024-02-03 18:38:45 +0500

    perf: ⚡️ migrate from gdbus to busctl command

--- puny-switcher.sh
+++ puny-switcher.sh
@@ -16,11 +16,15 @@
 # ============================================================================ #
 
 # Debug
-printf >&1 "\n"
+printf >&1 "==============================================================================\n"
 printf >&1 "puny-switcher started: primary   : %s\n" "$(xsel --output --primary -v)"
 printf >&1 "puny-switcher started: secondary : %s\n" "$(xsel --output --secondary -v)"
 printf >&1 "puny-switcher started: clipboard : %s\n" "$(xsel --output --clipboard -v)"
-printf >&1 "\n"
+printf >&1 "==============================================================================\n"
+
+# ============================================================================ #
+# Constants
+# ============================================================================ #
 
 # \x2F = / = slash
 # \x5C = \ = backslash
@@ -46,33 +50,70 @@ declare -A chardict
 chardict["us"]="$us1""$us2""$us3""$us4""$US1""$US2""$US3""$US4"
 chardict["ru"]="$ru1""$ru2""$ru3""$ru4""$RU1""$RU2""$RU3""$RU4"
 
+# Regex for non-empty string with at least one non-whitespace character
+NONSPACE="(.|\s)*\S(.|\s)*"
+# D-Bus interface path exposed by Gnome extension
+BUSCTL_PATH=('org.gnome.Shell' '/me/madhead/Shyriiwook' 'me.madhead.Shyriiwook')
+# busctl...currentLayout returns a string like: s "us"
+# grep -oP "\w+\s+\K.+" gets the quoted layout name: "us"
+BUSCTL_LAYOUT_FILTER="\w+\s+\K.+"
+# busctl...availableLayouts returns a string like: as 2 "us" "by"
+# grep -oP "\w+\s+\d+\s+\K.+" gets quoted layout names: "us" "by"
+BUSCTL_LAYOUTS_FILTER="\w+\s+\d+\s+\K.+"
+
+# ============================================================================ #
+# Functions
 # ============================================================================ #
 
 setlayout() {
-	# Switch keyboard layouts using Gnome extension
+	# Switch layout through a D-Bus interface exposed by Gnome extension
 	# 	$1: layout short name
+	busctl --user call "${BUSCTL_PATH[@]}" activate s "$1"
+}
+
+getlayout() {
+	# Get current keyboard layout name
+	# sed s/"\""/""/g removes quotes
+	echo -n "$(busctl --user get-property "${BUSCTL_PATH[@]}" \
+		currentLayout | grep -oP "$BUSCTL_LAYOUT_FILTER" | sed s/"\""/""/g)"
+}
+
+getlayouts() {
+	# Get an array of available layout names
+	# read -ra layouts <<<"mystring" reads whitespace-separated words into array
+	read -ra layouts <<<"$(busctl --user get-property "${BUSCTL_PATH[@]}" \
+		availableLayouts | grep -oP "$BUSCTL_LAYOUTS_FILTER" | sed s/"\""/""/g)"
+	# In Bash, variables created inside a function are kept by default, i.e.
+	# ${layouts} array will persist after function exit.
+}
+
+strconv() {
+	# sed y/$aaa/$bbb/ transliterates input string by replacing characters
+	# which appear in $aaa to corresponding characters in $bbb
+	echo -n "$(echo -n "$1" | sed y/"$2"/"$3"/)"
+}
+
+# ============================================================================ #
+# Alt implementations of functions for non-Gnome / non-systemd environments
+# ============================================================================ #
+
+setlayout-gdbus() {
 	gdbus call --session --dest org.gnome.Shell \
 		--object-path /me/madhead/Shyriiwook \
 		--method me.madhead.Shyriiwook.activate "$1" &>/dev/null
-	# TODO: replace with
-	# busctl --user call org.gnome.Shell /me/madhead/Shyriiwook me.madhead.Shyriiwook activate s "$1"
 }
 
 setlayout-nognome() {
 	setxkbmap "$1"
 }
 
-readdbus() {
-	# Get current and available keyboard layouts using Gnome extension
+readbus-gdbus() {
 	echo -n "$(gdbus introspect --session --dest org.gnome.Shell \
 		--object-path /me/madhead/Shyriiwook --only-properties)"
 	# TODO: replace with
-	# busctl --user get-property org.gnome.Shell /me/madhead/Shyriiwook me.madhead.Shyriiwook currentLayout
-	# busctl --user get-property org.gnome.Shell /me/madhead/Shyriiwook me.madhead.Shyriiwook availableLayouts
 }
 
-getlayout() {
-	# Get current keyboard layout name
+getlayout-gdbus() {
 	# 	$1: gdbus output text
 	echo -n "$(echo -n "$1" | grep -oP "currentLayout = '\K\w+")"
 }
@@ -83,16 +124,13 @@ getlayout-nognome() {
 	# https://askubuntu.com/q/973257/408821
 }
 
-getlayouts() {
+getlayouts-gdbus() {
 	# Get an array of available layout names
 	# 	$1: gdbus output text
 	# grep -oP "availableLayouts = \['\K\w+', '\w+" returns a string like "us', 'ru".
 	# sed s/"['|,]"/""/g removes apostrophes and commas.
-	# read -ra layouts <<< "$mystring" converts the string into an array.
 	read -ra layouts <<<"$(echo -n "$1" |
 		grep -oP "availableLayouts = \['\K\w+', '\w+" | sed s/"['|,]"/""/g)"
-	# In Bash, variables created inside a function are kept by default, i.e.
-	# ${layouts} array will persist after function exit.
 }
 
 getlayouts-nognome() {
@@ -101,22 +139,17 @@ getlayouts-nognome() {
 		grep -oP "layout: \s*\K\w*,\w*" | sed y/","/" "/)"
 }
 
-strconv() {
-	# sed y/$aaa/$bbb/ transliterates input string by replacing characters
-	# which appear in $aaa to corresponding characters in $bbb
-	echo -n "$(echo -n "$1" | sed y/"$2"/"$3"/)"
-}
-
+# ============================================================================ #
+# Main
 # ============================================================================ #
 
 case "$1" in
 
 get)
 	# Print available layouts, current layout, and its chars to stdout
-	gdbusstr=$(readdbus)
-	getlayouts "$gdbusstr"
+	getlayouts
 	printf >&1 "puny-switcher: available layouts: %s\n" "${layouts[@]}"
-	curlayout=$(getlayout "$gdbusstr")
+	curlayout=$(getlayout)
 	printf >&1 "puny-switcher: current layout: %s\n" "$curlayout"
 	srcchars="${chardict["$curlayout"]}"
 	printf >&1 "puny-switcher: layout characters: %s\n" "$srcchars"
@@ -129,17 +162,15 @@ set)
 
 iset)
 	# Set keyboard layout by index: 0|1
-	gdbusstr=$(readdbus)
-	getlayouts "$gdbusstr"
+	getlayouts
 	newlayout="${layouts["$2"]}"
 	setlayout "$newlayout"
 	;;
 
 switch)
 	# Switch current layout
-	gdbusstr=$(readdbus)
-	getlayouts "$gdbusstr"
-	curlayout=$(getlayout "$gdbusstr")
+	getlayouts
+	curlayout=$(getlayout)
 
 	if [[ "$curlayout" == "${layouts[0]}" ]]; then
 		newlayout=${layouts[1]}
@@ -158,13 +189,10 @@ convert)
 	# Get content of the primary buffer, i.e. latest active selection made in any window
 	instr=$(xsel --output --primary)
 
-	NONSPACE="(.|\s)*\S(.|\s)*"
 	if [[ $instr =~ $NONSPACE ]]; then
-		# Selection is non-empty and has non-whitespace characters
 
-		gdbusstr=$(readdbus)
-		getlayouts "$gdbusstr"
-		curlayout=$(getlayout "$gdbusstr")
+		getlayouts
+		curlayout=$(getlayout)
 
 		if [[ "$curlayout" == "${layouts[0]}" ]]; then
 			newlayout=${layouts[1]}
@@ -204,7 +232,7 @@ restore)
 	;;
 
 *)
-	printf >&2 'puny-switcher: error: unknown argument: %s\n' "$1"
+	printf >&2 "puny-switcher: error: unknown argument: %s\n" "$1"
 	exit 1
 	;;
 esac
@@ -212,10 +240,10 @@ esac
 # ============================================================================ #
 
 # Debug
-printf >&1 "\n"
+printf >&1 "==============================================================================\n"
 printf >&1 "puny-switcher exiting: primary   : %s\n" "$(xsel --output --primary -v)"
 printf >&1 "puny-switcher exiting: secondary : %s\n" "$(xsel --output --secondary -v)"
 printf >&1 "puny-switcher exiting: clipboard : %s\n" "$(xsel --output --clipboard -v)"
-printf >&1 "\n"
+printf >&1 "==============================================================================\n"
 
 exit 0
